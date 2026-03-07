d91132e762d979e426c7405d91474b4204f8601f
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-03-07 15:13:14 +0530

    feat(showreel): stub isolated dark-theme video content section

---
+++ components/Showreel.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Showreel() {
+    return (
+        <section className="showreel-section" id="showreel-section">
+            <div className="showreel__content">
+                <h2 className="showreel__title">Showreel Section</h2>
+                <p className="showreel__subtitle">Will be created soon</p>
+            </div>
+        </section>
+    );
+}
